b43bcdb0b6bb7f350e475331b6152e8afb486da9
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2020-10-08 11:18:14 +0200

    updated game repository

--- src/main/java/com/nickleback/quartettBackend/repository/GameRepository.java
+++ src/main/java/com/nickleback/quartettBackend/repository/GameRepository.java
@@ -3,7 +3,9 @@ package com.nickleback.quartettBackend.repository;
 import com.nickleback.quartettBackend.domain.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GameRepository extends JpaRepository<Game, UUID> {
+    Optional<Game> findByInviteCode(Long gameInviteCode);
 }
