55b7e62d49fe931692e8ea98b0a1427751d2840e
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2020-10-20 10:29:37 +0200

    added websocket authentication ability

--- src/main/java/com/nickleback/quartettBackend/security/JWTAuthorizationFilter.java
+++ src/main/java/com/nickleback/quartettBackend/security/JWTAuthorizationFilter.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.JWT;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -14,6 +13,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
@@ -27,31 +28,28 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String header = request.getHeader(SecurityConstants.HEADER_STRING);
+        String param = request.getParameter(SecurityConstants.HEADER_STRING);
 
-        if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+        if ((header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) && (param == null || !param.startsWith(SecurityConstants.TOKEN_PREFIX))) {
             chain.doFilter(request, response);
             return;
         }
 
-        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(Objects.requireNonNullElse(header, param));
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
 
         chain.doFilter(request, response);
     }
 
-    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
-        String token = request.getHeader(SecurityConstants.HEADER_STRING);
+    public UsernamePasswordAuthenticationToken getAuthentication(String token){
         if (token != null) {
-            // parse the token.
-            String username = JWT.require(Algorithm.HMAC256(SecurityConstants.SECRET.getBytes()))
+            String user = JWT.require(Algorithm.HMAC256(SecurityConstants.SECRET.getBytes()))
                     .build()
                     .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""))
                     .getSubject();
-
-            if (username != null) {
-                UserDetails user = userDetailsService.loadUserByUsername(username);
-                return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
             }
             return null;
         }
